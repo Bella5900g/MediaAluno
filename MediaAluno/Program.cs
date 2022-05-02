96453c3c1f5bf8632b01465b8e9d290fb710f4bc
Isabella Vieira <100964780+Bella5900g@users.noreply.github.com>
2022-05-02 03:02:07 +0200

    Add files via upload

---
+++ MediaAluno/Program.cs
@@ -0,0 +1,25 @@
+﻿class Colegio
+{
+    static void Main(){
+        double nota1,nota2;
+        double media;
+        string nome;
+
+        Console.WriteLine("Digite seu nome: ");
+        nome=Console.ReadLine();
+        Console.WriteLine("Digite a primeira nota> ");
+        nota1=Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite sua segunda nota: ");
+        nota2=Convert.ToDouble(Console.ReadLine());
+
+        media = (nota1 + nota2) / 2;
+
+        if(media >=6){
+            Console.WriteLine($"Parabéns {nome} você foi APROVADO! ");
+        }
+        else{
+            Console.WriteLine($"Infelizmente você foi REPROVADO {nome} ");
+        }
+        Console.ReadKey();
+    }
+}
